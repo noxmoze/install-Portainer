db283784ba8fb3f53a4061b4ed17cce1549c7082
noxmoze <84128804+noxmoze@users.noreply.github.com>
2021-08-12 01:12:52 +0200

    Add files via upload

---
+++ Portainer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+
+sudo apt install docker-ce
+
+docker volume create portainer_data
+
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
